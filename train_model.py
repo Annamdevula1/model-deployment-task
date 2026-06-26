207c0fa9ed1855a4d5a60ee10b450a47665b2eff
ANNAMDEVULA DURGA PRASAD <durgaprasadannamdevula41@gmail.com>
2026-06-26 14:41:48 +0530

    Add model training and saving functionality

---
+++ train_model.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+# Load dataset
+df = pd.read_csv("data.csv")
+
+# Select features and target
+X = df[['bedrooms', 'bathrooms', 'sqft_living',
+        'sqft_lot', 'floors', 'waterfront',
+        'view', 'condition', 'sqft_above',
+        'sqft_basement', 'yr_built', 'yr_renovated']]
+
+y = df['price']
+
+# Train model
+model = LinearRegression()
+model.fit(X, y)
+
+# Save model
+joblib.dump(model,("trained_model.pkl"))
+
+print("Model saved successfully")
